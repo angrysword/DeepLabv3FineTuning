002f62e2aef70faa19a0e4c7edbb54420eb5dc93
angrysword <nion.cao@gmail.com>
2021-01-13 18:16:09 -0800

    change only load pretrain model number class with

--- model.py
+++ model.py
@@ -13,8 +13,8 @@ def createDeepLabv3(outputchannels=1):
     Returns:
         model: Returns the DeepLabv3 model with the ResNet101 backbone.
     """
-    model = models.segmentation.deeplabv3_resnet101(pretrained=True,
-                                                    progress=True)
+    model = models.segmentation.deeplabv3_resnet101(pretrained=False,
+                                                    progress=True,num_classes=2)
     model.classifier = DeepLabHead(2048, outputchannels)
     # Set the model in training mode
     model.train()
